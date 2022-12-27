62ed4c083da797ada73ef35693e3474ffe8215bc
Prof. Leonardo Melo <90953714+profleonardomelo@users.noreply.github.com>
2022-12-27 16:10:35 -0300

    Comentando código de classe

--- Java_21_ProgramaExercicioLogin/ProgramaExercicioLogin_C.java
+++ Java_21_ProgramaExercicioLogin/ProgramaExercicioLogin_C.java
@@ -13,26 +13,38 @@ digitou o login e senha incorretos por duas vezes,
 restando apenas uma última chance.
 */
 
+//Define o pacote que contém esta classe
 package Java_21_ProgramaExercicioLogin;
 
+//Importando Classe
 import javax.swing.JOptionPane;
 
+//Cria uma classe pública chamada "ProgramaExercicioLogin_C"
 public class ProgramaExercicioLogin_C {
 
+    //Cria o método principal (função) "main"
     public static void main(String[] args) {
 
+        //Cria uma estrutura de repetição (for) que repete um código interno três vezes
         for (int i = 1; i <= 3; i++)
         {
+            //Lê, em uma caixa de diálogo, um texto digitado pelo usuário e guarda em uma variável
             String login = JOptionPane.showInputDialog("Forneça um Login: ");
+            //Lê, em uma caixa de diálogo, um texto digitado pelo usuário e guarda em uma variável
             String senha = JOptionPane.showInputDialog("Forneça uma Senha: ");
 
+            //Se login for igual a 'java' e senha for igual a 'java', faça:
             if (login.equals("java") && senha.equals("java")) {
+                //Mostra caixa de diálogo com mensagem de sucesso.
                 JOptionPane.showMessageDialog(null, "Login e senha aceitos.");
+                //Interrompe a interações da estrutura de repetição for
                 break;
             }
             else
             {
+                //Concatena e armazena texto que será exibe em caso de falha no acesso ao sistema (com número de tentativas).
                 String mensagem = "Falha, verifique login e senha.\nVocê tem mais " + (3 - i) + " tentativa(s).";
+                //Mostra caixa de diálogo com mensagem de falha.
                 JOptionPane.showMessageDialog(null, mensagem);
             }
         }
